3a1ace4cc7681825613e82190300e00a3101feda
Ferreyrafg <fgf14112002@gmail.com>
2026-07-04 01:36:45 -0300

    Creacion del repository de usuario

---
+++ src/main/java/com/federico/Ecommerce/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.federico.Ecommerce.repositories;
+
+import com.federico.Ecommerce.models.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+       Optional<Usuario> findById(Integer id);//SELECT * FROM usuario WHERE id = ?;
+}
