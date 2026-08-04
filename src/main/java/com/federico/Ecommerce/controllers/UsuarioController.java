5fbd490f8ccf4adec74dd4702de95f75556e4c5e
ferreyrafg-14 <ferreyrafg30@gmail.com>
2026-08-04 17:14:47 -0300

    Feature/UsurioCrud: Implementación @Getmapping que devuelve todos los usuarios

--- src/main/java/com/federico/Ecommerce/controllers/UsuarioController.java
+++ src/main/java/com/federico/Ecommerce/controllers/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -26,8 +27,14 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> findAll(){
+        List<UsuarioResponseDto> response = service.ObtenerUsuarios();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @GetMapping("{id}")
-    public ResponseEntity<UsuarioResponseDto> ObtenerUsuario(@PathVariable Integer id){
+    public ResponseEntity<UsuarioResponseDto> findById(@PathVariable Integer id){
         UsuarioResponseDto response = service.obtenerUsuarioId(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
--- src/main/java/com/federico/Ecommerce/services/UsuarioService.java
+++ src/main/java/com/federico/Ecommerce/services/UsuarioService.java
@@ -9,6 +9,9 @@ import com.federico.Ecommerce.repositories.UsuarioRepository;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 
@@ -33,8 +36,25 @@ public class UsuarioService {
 
      public UsuarioResponseDto obtenerUsuarioId(Integer id){
          Usuario usuarioObtenido = repository.findById(id)
-                 .orElseThrow(() -> new ResourceNotFoundException("No se ha encontrado usuario con ID: " + id)) ;
+                 .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado ")) ;
          return usuariomapper.toResponseDto(usuarioObtenido);
+     }
+
+     public List<UsuarioResponseDto> ObtenerUsuarios(){
+            List<Usuario> listaUsuarios = repository.findAll();
+
+            if(listaUsuarios.isEmpty()){
+                throw new ResourceNotFoundException("Usuarios no encontrados");
+            }
+
+            List<UsuarioResponseDto> listaResponseDto = new ArrayList<>();
+
+            for(Usuario usuario : listaUsuarios){
+                listaResponseDto.add(usuariomapper.toResponseDto(usuario));
+            }
+
+            return  listaResponseDto;
+
      }
 
 
