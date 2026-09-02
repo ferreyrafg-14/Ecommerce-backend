6d54820379155c8d302fa659f6f5789ec477291f
ferreyrafg-14 <ferreyrafg30@gmail.com>
2026-09-02 15:16:06 -0300

    Se añadio atributo DNI de tipo Integer al model USER y se corrigio nombre de archivos de CartItemrepository y OrderItemrepository

--- src/main/java/com/federico/Ecommerce/models/Entity/User.java
+++ src/main/java/com/federico/Ecommerce/models/Entity/User.java
@@ -33,6 +33,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Rol role;
 
+    @Column(name = "dni" , nullable = false)
+    private Integer dni;
+
     @CreationTimestamp
     @Column(name = "fechacreacion", nullable = false)
     private LocalDateTime createdAt;
@@ -45,7 +48,7 @@ public class User {
 
     public User(Integer userId, String firstname, String lastname,
                 String email, String password, Rol role,
-                LocalDateTime createdAt) {
+                LocalDateTime createdAt , Integer dni) {
         this.userId = userId;
         this.firstname = firstname;
         this.lastname = lastname;
@@ -53,6 +56,7 @@ public class User {
         this.password = password;
         this.role = role;
         this.createdAt = createdAt;
+        this.dni = dni;
     }
 
     public Integer getUserId() {
@@ -111,15 +115,34 @@ public class User {
         this.createdAt = createdAt;
     }
 
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
     @Override
-    public String toString() {
+    public String  toString() {
         return "User{" +
                 "userId=" + userId +
                 ", firstname='" + firstname + '\'' +
                 ", lastname='" + lastname + '\'' +
                 ", email='" + email + '\'' +
-                ", role='" + role + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                ", dni=" + dni +
                 ", createdAt=" + createdAt +
+                ", orders=" + orders +
                 '}';
     }
 }
