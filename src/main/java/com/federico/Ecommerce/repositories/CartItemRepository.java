acc89791d68bbccddc0d7bf3833936ad24fe2e29
ferreyrafg-14 <ferreyrafg30@gmail.com>
2026-09-02 15:18:40 -0300

    Se corrigio tipo de dato en el repository

--- src/main/java/com/federico/Ecommerce/repositories/CartItemRepository.java
+++ src/main/java/com/federico/Ecommerce/repositories/CartItemRepository.java
@@ -1,8 +1,9 @@
 package com.federico.Ecommerce.repositories;
 
+import com.federico.Ecommerce.models.Entity.CartItem;
 import com.federico.Ecommerce.models.embeddable.CartItemId;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartItem extends JpaRepository<CartItem, CartItemId> {
+public interface CartItemRepository extends JpaRepository<CartItem, CartItemId> {
 }
--- src/main/java/com/federico/Ecommerce/repositories/OrderItemRepository.java
+++ src/main/java/com/federico/Ecommerce/repositories/OrderItemRepository.java
@@ -1,7 +1,8 @@
 package com.federico.Ecommerce.repositories;
 
+import com.federico.Ecommerce.models.Entity.OrderItem;
 import com.federico.Ecommerce.models.embeddable.OrderItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItem extends JpaRepository<OrderItem, OrderItemId> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemId> {
 }
